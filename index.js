1ac91a224bf45366818a34c0a17e573454070d84
Yin <doyin@deloitte.com.cn>
2024-04-03 14:16:29 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const roundNumber = require('./roundNumber')
+const randomColor = require('./randomColor')
+const getMaxValue = require('./getMaxValue')
+const isPalindrome = require('./isPalindrome')
+const reverseString = require('./reverseString')
+
+module.exports = (str) => {
+    return str.toUpperCase();
+  }
